7e0c40c3a5a3dffbd95e8952f193b92022a07a52
moniruzzaman <md.moniruzzaman1050@gmail.com>
2021-11-07 02:25:12 +0600

    add signUp page to callect data

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
-let inputName =prompt("type your name");
-let inputAGE =prompt("type your age");
-let inputId =prompt("type your collage id no.");
-// let inputrelgon =prompt("type your religion");
 
+let submitBtnEl=document.querySelector('#btn11');
+submitBtnEl.addEventListener('click',function add(){
+    document.querySelector('.login').style.display = "none";
+     document.querySelector('#profilePage').style.display = "inline-block";
+    
+   let inputName1=document.getElementById("inputFromName").value;
+    let inputAGE =document.getElementById("inputFromage").value;
+    let Id =document.getElementById("inputFromId").value;
+   
 
 
 let ProfileName = document.querySelector('#ProfileName');
@@ -14,12 +19,8 @@ let ProfileAge = document.querySelector('#Profileage');
 let country = document.querySelector('#country');
 let Relation = document.querySelector('#Relation');
 let Religon = document.querySelector('#Religon');
-
-class studentprofile{
-    constructor(name,id,age){
-        this.name= name;
-        this.id = id;
-        this.age = age;
+class others {
+    constructor(){
         this.collage = "Govt. bangla collage";
         this.country = "Bangladeshi";
         this.Relation = "Single";
@@ -27,8 +28,19 @@ class studentprofile{
 
     }
 }
+class studentprofile extends others{
+    constructor(name,id,age){
+        super();
+        this.name= name;
+        this.id = id;
+        this.age = age;
+        
+       
 
-let newStudent = new studentprofile(inputName,inputId,inputAGE);
+    }
+}
+
+let newStudent = new studentprofile(inputName1,inputAGE,Id);
 ProfileName.innerText=newStudent.name;
 ProfileID.innerText =newStudent.id;
 ProfileAge.innerText =newStudent.age;
@@ -36,3 +48,11 @@ country.innerText = newStudent.country;;
 Relation.innerText = newStudent.Relation;
 Profileclz.innerText =newStudent.collage;
 Religon.innerText =newStudent.Religon;
+})
+document.querySelector('#button2').addEventListener('click',(e)=>{
+    document.querySelector('.login').style.display = "inline-block";
+    document.querySelector('#profilePage').style.display = "none";
+  document.getElementById("inputFromName").value='';
+    document.getElementById("inputFromage").value='0';
+    document.getElementById("inputFromId").value='0';
+})
