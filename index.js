f0e4a89a5df05eea00b283dd648a5cb717304bd6
moniruzzaman <md.moniruzzaman1050@gmail.com>
2021-11-07 19:29:22 +0600

    fixed responsiveness

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 
+
+
+
+
+
+
+function add() {
+
+
+
+
+
+    
 let ProfileName = document.querySelector('#ProfileName');
 let ProfileID = document.querySelector('#ProfileID');
 let Profileclz = document.querySelector('#ProfileCollage');
@@ -9,32 +22,29 @@ let Relation = document.querySelector('#Relation');
 let Religon = document.querySelector('#Religon');
 
 
-const inputName1=document.getElementById("inputFromName").value;
+var inputName1=document.getElementById("inputFromName").value;
 const inputAGE =document.getElementById("inputFromage").value;
 const Id =document.getElementById("inputFromId").value;
 
 
 
 
-
-
-let submitBtnEl=document.querySelector('#btn11');
-submitBtnEl.addEventListener('click',function add(){
-   let showerror = document.querySelector('#inputFromName').value;
- 
+    
+    let showerror = document.querySelector('#inputFromName').value;
   
    
+    
     if( !showerror) {
         
        document.querySelector('.firstHeading').innerHTML="Type name";
         
-
+ 
     }else if(showerror){
+
         document.querySelector('.login').style.display = "none";
         document.querySelector('#profilePage').style.display = "inline-block";
+ 
 
-
-   
         class others {
             constructor(){
                 this.collage = "Govt. bangla collage";
@@ -55,24 +65,40 @@ submitBtnEl.addEventListener('click',function add(){
         
             }
         }
-        
       
+ 
+ 
+    var newStudent = new studentprofile(inputName1,inputAGE,Id);
+    inputName1=newStudent.name;
+    ProfileName.innerText=newStudent.name;
+    ProfileID.innerText =newStudent.id;
+    ProfileAge.innerText =newStudent.age;
+    country.innerText = newStudent.country;;
+    Relation.innerText = newStudent.Relation;
+    Profileclz.innerText =newStudent.collage;
+    Religon.innerText =newStudent.Religon;
 
 
-let newStudent = new studentprofile(inputName1,inputAGE,Id);
-ProfileName.innerText=newStudent.name;
-ProfileID.innerText =newStudent.id;
-ProfileAge.innerText =newStudent.age;
-country.innerText = newStudent.country;;
-Relation.innerText = newStudent.Relation;
-Profileclz.innerText =newStudent.collage;
-Religon.innerText =newStudent.Religon;
+
+  
     }
-})
+  
+ }
+ 
+
+let submitBtnEl=document.querySelector('#btn11');
+submitBtnEl.addEventListener('click', add );
+
 document.querySelector('#button2').addEventListener('click',(e)=>{
     document.querySelector('.login').style.display = "inline-block";
     document.querySelector('#profilePage').style.display = "none";
+    add();
   document.getElementById("inputFromName").value='';
     document.getElementById("inputFromage").value='';
     document.getElementById("inputFromId").value='';
+   
 })
+
+
+
+ 
