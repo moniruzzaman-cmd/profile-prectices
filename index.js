b55b021bbf31b850298a3c4883f662bd2b2f9b1c
moniruzzaman <md.moniruzzaman1050@gmail.com>
2021-11-07 14:11:02 +0600

    fixed log in value bug

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 
-
-let submitBtnEl=document.querySelector('#btn11');
-submitBtnEl.addEventListener('click',function add(){
-    document.querySelector('.login').style.display = "none";
-     document.querySelector('#profilePage').style.display = "inline-block";
-    
-   let inputName1=document.getElementById("inputFromName").value;
-    let inputAGE =document.getElementById("inputFromage").value;
-    let Id =document.getElementById("inputFromId").value;
-   
-
-
 let ProfileName = document.querySelector('#ProfileName');
 let ProfileID = document.querySelector('#ProfileID');
 let Profileclz = document.querySelector('#ProfileCollage');
@@ -19,26 +7,57 @@ let ProfileAge = document.querySelector('#Profileage');
 let country = document.querySelector('#country');
 let Relation = document.querySelector('#Relation');
 let Religon = document.querySelector('#Religon');
-class others {
-    constructor(){
-        this.collage = "Govt. bangla collage";
-        this.country = "Bangladeshi";
-        this.Relation = "Single";
-        this.Religon = "Islam";
 
-    }
-}
-class studentprofile extends others{
-    constructor(name,id,age){
-        super();
-        this.name= name;
-        this.id = id;
-        this.age = age;
+
+const inputName1=document.getElementById("inputFromName").value;
+const inputAGE =document.getElementById("inputFromage").value;
+const Id =document.getElementById("inputFromId").value;
+
+
+
+
+
+
+let submitBtnEl=document.querySelector('#btn11');
+submitBtnEl.addEventListener('click',function add(){
+   let showerror = document.querySelector('#inputFromName').value;
+ 
+  
+   
+    if( !showerror) {
         
-       
+       document.querySelector('.firstHeading').innerHTML="Type name";
+        
 
-    }
-}
+    }else if(showerror){
+        document.querySelector('.login').style.display = "none";
+        document.querySelector('#profilePage').style.display = "inline-block";
+
+
+   
+        class others {
+            constructor(){
+                this.collage = "Govt. bangla collage";
+                this.country = "Bangladeshi";
+                this.Relation = "Single";
+                this.Religon = "Islam";
+        
+            }
+        }
+        class studentprofile extends others{
+            constructor(name,id,age){
+                super();
+                this.name= name;
+                this.id = id;
+                this.age = age;
+                
+               
+        
+            }
+        }
+        
+      
+
 
 let newStudent = new studentprofile(inputName1,inputAGE,Id);
 ProfileName.innerText=newStudent.name;
@@ -48,6 +67,7 @@ country.innerText = newStudent.country;;
 Relation.innerText = newStudent.Relation;
 Profileclz.innerText =newStudent.collage;
 Religon.innerText =newStudent.Religon;
+    }
 })
 document.querySelector('#button2').addEventListener('click',(e)=>{
     document.querySelector('.login').style.display = "inline-block";
